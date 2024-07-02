5dfb0e142da40f5a57268d51c28c480483811cee
Anil Kumar <anil582905@gmail.com>
2024-07-02 14:13:23 +0530

    Some modification

--- app.js
+++ app.js
@@ -1,35 +1,31 @@
-let box1=document.querySelector(".box1");
-let box2=document.querySelector(".box2");
+let box1 = document.querySelector(".content1");
+let box2 = document.querySelector(".content2");
+let buttons = Array.from(document.getElementsByClassName('btn'));
 
-document.addEventListener("click",function(event){
-    // console.log(event.target.innerText);
-    switch(event.target.innerText){
-        case 'C':{
-            box2.innerText=" ";
-            box1.innerText=" ";
-            break;
-        }
-        case '<-':{
-            if(box2.innerText){
-                box2.innerText=box2.innerText.slice(0,-1);
-            }
-            break;
-        }
-        case '=':{
-            box1.innerText=box2.innerText;
-            box2.innerText=" ";
-                try{
-                    box2.innerText=eval(box1.innerText);
-                }
-                catch{
-                    box2.innerText="error";
+buttons.map((button) => {
+    button.addEventListener("click", (e) => {
+        switch (e.target.innerText) {
+            case 'C':
+                box1.innerText = '';
+                box2.innerText = '';
+                break;
+            case 'D':
+                if (box1.innerText) {
+                    box1.innerText = box1.innerText.slice(0, -1);
                 }
                 break;
+            case '=':
+                try {
+                    box2.innerText = box1.innerText;
+                    box1.innerText = eval(box1.innerText);
+                } catch {
+                    box1.innerText = "Error";
+                }
+                break;
+            default:
+                box1.innerText = box1.innerText + e.target.innerText;
+                break;  // Add break to prevent fall-through
         }
-        default:{
-            box2.innerText=box2.innerText+event.target.innerText;
-        }
-
-    }
-
-})
+        box1.scrollLeft = 0;
+    });
+});
